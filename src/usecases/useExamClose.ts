8ead7f155c7242a670e1adb79e59954d5caf5c3e
tobias-tj <https.tobias.jara.404@gmail.com>
2025-03-26 09:16:41 -0300

    Request para enviar para cerrar el examen

--- src/usecases/useExamClose.ts
+++ src/usecases/useExamClose.ts
@@ -12,7 +12,7 @@ export const useCloseExam = async (
     // Production: http://161.35.53.140:8888/local/quiz_closer/index.php
     const response = await axios.post(
       `http://161.35.53.140:8888/local/quiz_closer/index.php`,
-      { quizid, attemptId, cmid }, // Enviamos el JSON en el cuerpo
+      { quizid, attemptid: attemptId, cmid },
       {
         headers: {
           "Content-Type": "application/json",
